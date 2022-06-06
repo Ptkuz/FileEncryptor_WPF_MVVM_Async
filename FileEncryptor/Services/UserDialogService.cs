9997000592c569037c410a082eff9cd8ded6312f
Gurrex <ptkuz@yandex.ru>
2022-06-06 15:26:27 +0700

    Диалог с пользователем выбора файла

--- FileEncryptor/Services/UserDialogService.cs
+++ FileEncryptor/Services/UserDialogService.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FileEncryptor.Services.Interfaces;
+using Microsoft.Win32;
+
 
 namespace FileEncryptor.Services
 {
     internal class UserDialogService : IUserDialog
     {
+        public bool OpenFile(string title, out string selectedFile, string filter = "Все файлы (*.*)|(*.*)")
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog() 
+            {
+                Title = title,
+                Filter = filter, 
+            };
 
+            if (fileDialog.OpenFile() == null) 
+            {
+                selectedFile = null;
+                return false;
+            }
+            selectedFile = fileDialog.FileName;
+            return true;
+        }
+
+        public bool OpenFiles(string title, out IEnumerable<string> selectedFiles, string filter = "Все файлы (*.*)|(*.*)")
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog()
+            {
+                Title = title,
+                Filter = filter,
+            };
+        }
     }
 }
